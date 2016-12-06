30a9c09b3f45eed15cec43b179e8a26955c18f01
Brad Frost <bfozzy@icloud.com>
2016-12-05 23:10:53 -0700

    added toy problem

--- CodeWars/7Kata.js
+++ CodeWars/7Kata.js
@@ -56,3 +56,50 @@ function accum(s) {
 function accum(s) {
   return s.split('').map((x,index) => x.toUpperCase()+Array(index+1).join(x.toLowerCase())).join('-');
 };
+
+// Your task is to write function toLeetSpeak that converts a regular english sentence to Leetspeak.
+//
+// More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+//
+// Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+
+function toLeetSpeak(str) {
+var alph = {
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2'
+}
+str = str.split('');
+for (var i = 0; i < str.length; i++){
+for (var prop in alph){
+if (str[i] === prop){
+str[i] = alph[prop];
+}
+}
+}
+  return str.join('');
+}
